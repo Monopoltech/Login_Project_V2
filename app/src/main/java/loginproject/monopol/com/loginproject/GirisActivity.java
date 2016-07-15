8acd45ffe160eab8f345aacc49b266f75354b9ea
Monopoltech <developer@monopolyonetim.com>
2016-07-15 16:32:29 +0300

    Cağan

--- app/src/main/java/loginproject/monopol/com/loginproject/GirisActivity.java
+++ app/src/main/java/loginproject/monopol/com/loginproject/GirisActivity.java
@@ -24,6 +24,8 @@ public class GirisActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+
             }
         });
     }
